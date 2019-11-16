e9871242e28028a7e5ce1c84021d8ebf6c070dea
aymen <bensalah.aymen@asmhost.com>
2019-11-16 20:20:33 +0100

    fix candite add bugs

--- app/Http/Controllers/CandidateCotroller.php
+++ app/Http/Controllers/CandidateCotroller.php
@@ -4,20 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 class CandidateCotroller extends Controller
 {
     public function addCandidate(Request $request)
     {
-            $response = DB::table("candidate")->insert([
-                            "firstname"=>$request->get("firstname"),
-                            "lastname"=>$request->get("lastname"),	
-                            "email"=>$request->get("email")	,
-                            "password"=>$request->get("password"),
-                            "role"=>$request->get("role"),
-                            "permis_id"=>$request->get("permis_id"),
-                            "created_at"=>$request->get("created_at"),
-                            "updated_at"=>$request->get("updated_at")
-
-            ])
+        $response = DB::table('candidate')->insert([
+                            'firstname' => $request->get('firstname'),
+                            'lastname' => $request->get('lastname'),
+                            'email' => $request->get('email'),
+                            'password' => $request->get('password'),
+                            'role' => $request->get('role'),
+                            'permis_id' => $request->get('permis_id'),
+                            'created_at' => $request->get('created_at'),
+                            'updated_at' => $request->get('updated_at'),
+            ]);
     }
 }
